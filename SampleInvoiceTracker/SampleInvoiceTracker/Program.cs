bad3be58cfe800d81eaa14a0e5fdb636db1349ba
Kailash B <kaish95@gmail.com>
2020-04-03 13:30:33 +0530

    Refactored Program.cs
    Just a minor refactoring in Program.cs

--- SampleInvoiceTracker/SampleInvoiceTracker/Program.cs
+++ SampleInvoiceTracker/SampleInvoiceTracker/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SampleInvoiceTracker
 {
@@ -12,16 +8,20 @@ namespace SampleInvoiceTracker
             Console.WriteLine("Hi There!");
             Console.WriteLine(
                 "This is a sample Console Application just to know how to add stuff to GitHub");
-            while (true) {
-                Console.WriteLine("Press K to open my GitHub profile...");
-                Console.WriteLine("Press any other key to exit");
-                var choice = Console.ReadLine();
-                if (choice == "K") {
-                    Process.Start("https://github.com/kailash-b?tab=repositories");
-                } else {
-                    break;
-                }
-                Console.ReadKey();
+            while (Show()) {
+            }
+            Console.ReadKey();
+        }
+
+        private static bool Show() {
+            Console.WriteLine("Press K to open my GitHub profile...");
+            Console.WriteLine("Press any other key to exit");
+            var choice = Console.ReadLine();
+            if (choice == "K") {
+                Process.Start("https://github.com/kailash-b?tab=repositories");
+                return true;
+            } else {
+                return false;
             }
         }
     }
